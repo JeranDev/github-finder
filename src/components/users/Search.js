189dfbf13435d875e6e0cb914fdf759fc3565ca4
JeranDev <jeranjb@gmail.com>
2021-11-03 13:47:13 -0600

    add clearUsers functionality

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   }
 
   onSubmit = e => {
@@ -19,21 +21,30 @@ class Search extends Component {
   onChange = e => this.setState({ [e.target.name]: e.target.value })
 
   render() {
+    const { showClear, clearUsers } = this.props
+
     return (
-      <form className='form' onSubmit={this.onSubmit}>
-        <input
-          type='text'
-          name='text'
-          placeholder='Search Users...'
-          value={this.state.text}
-          onChange={this.onChange}
-        />
-        <input
-          type='submit'
-          value='Search'
-          className='btn btn-dark btn-block'
-        />
-      </form>
+      <div>
+        <form className='form' onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
+      </div>
     )
   }
 }
